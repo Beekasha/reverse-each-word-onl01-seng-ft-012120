9fc190c8f8208b6c3f36e8f6bc5802ccedc84345
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-01-23 02:30:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  new_sentence = []
-  sentence = string.split("") #turn string into an array
+  #new_sentence = []
+  #sentence = string.split("") #turn string into an array
   
-  sentence.each do { |letter| new_sentence.unshift(letter)}
-  new_sentence.join("")
+  #sentence.each do { |letter| new_sentence.unshift(letter)}
+  #new_sentence.join("")
 end
